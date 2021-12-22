9d77e62cd8b4946f458641e5bf8ea92ad09c9468
unknown <wabrodor@gmail.com>
2021-12-22 19:20:55 +0200

    add

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -7,7 +7,6 @@ const ejs = require('ejs');
 let people = ['geddy', 'neil', 'alex'];
 
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 // app.set('views', path.join(__dirname, "views")); 
 app.set('view engine', 'ejs');
