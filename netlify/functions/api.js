2d821fad2748e911e38a70d7a82b345ed516fb2f
unknown <wabrodor@gmail.com>
2021-12-22 20:55:28 +0200

    push

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -8,7 +8,7 @@ const ejs = require('ejs');
 let people = ['geddy', 'neil', 'alex'];
 
 app.use(express.static( "./public"))
-app.set('./views',  path.join(__dirname, "../views")); 
+app.set('../views',  path.join(__dirname, "views")); 
 app.set('view engine', 'ejs');
 
 router.get("/", async  (req, res)=>{
@@ -19,7 +19,6 @@ router.get("/", async  (req, res)=>{
             }
         })
         const data = await response.json()
-        console.log(data)
         res.render("index.ejs", {people:data})
     }catch (error) {console.log(error)}
 
