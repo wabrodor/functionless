6585d4f576804d7afdb2b452f480296bef7eb1f6
unknown <wabrodor@gmail.com>
2021-12-22 20:38:19 +0200

    update

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,26 +2,37 @@ const express = require("express")
 const serverless = require("serverless-http")
 const router =  express.Router()
 const app = express();
+const fetch = require('node-fetch');
 const path = require('path');
 const ejs = require('ejs');
 let people = ['geddy', 'neil', 'alex'];
 
-
-
-// app.set('views', path.join(__dirname, "views")); 
+app.use(express.static( "./public"))
+app.set('./views',  path.join(__dirname, "./views")); 
 app.set('view engine', 'ejs');
 
-router.get("/", function(req, res){
-    res.render("index", {people:people})
+router.get("/", async  (req, res)=>{
+    try{
+        const response = await fetch("https://icanhazdadjoke.com/", {
+            headers:{
+      Accept:"application/json "
+            }
+        })
+        const data = await response.json()
+        console.log(data)
+        res.render("index", {people:data})
+    }catch (error) {console.log(error)}
+
+
 })
 
 router.get("/birds", (req, res) =>{
     res.send("worked")
 })
 
-router.get("*", (req, res) =>{
-    res.send("hend")
-})
+// router.get("*", (req, res) =>{
+//     res.send("hend")
+// })
 app.use("/",   router)
 
 module.exports.handler = serverless(app)
